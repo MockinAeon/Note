19ece345d5c9205cfa57ff3676d87226e5a5f465
Ubuntu <ubuntu@ip-172-31-29-111.us-west-2.compute.internal>
2017-11-15 05:56:33 +0000

    init login with some notes files

---
+++ imports/ui/Dashboard.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+import PrivateHeader from './PrivateHeader.js';
+import NoteList from './NoteList.js';
+
+export default () => {
+  return (
+    <div>
+      <PrivateHeader title="Dashboard" />
+      <div className="page-content">
+        <NoteList />
+      </div>
+    </div>
+  );
+}
+
